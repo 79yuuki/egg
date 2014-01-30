e2b06dc1354e585933c4bbede15b58c12a7179c0
Yuuki Shichiku <yuuki.79@gmail.com>
2014-01-31 03:56:48 +0900

    Add view.js

--- public/javascripts/draw.js
+++ public/javascripts/draw.js
@@ -100,7 +100,7 @@ window.addEventListener("load", function () {
         once = false;
         s.emit('throw', {x: gv.x, y: gv.y});
         $('canvas').animate({ 'margin-top': '-999px'}, 'fast');
-        setTimeout(location.reload(), 1500);
+        setTimeout(location.reload(), 3000);
       }
     }
   });
