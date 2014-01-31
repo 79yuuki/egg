75b63918216b1def65475043154672e61ba7945e
Yuuki Shichiku <yuuki.79@gmail.com>
2014-01-31 22:35:50 +0900

    Add egg demow

--- public/javascripts/view.js
+++ public/javascripts/view.js
@@ -127,13 +127,7 @@ loadImages(eggPosiImg, eggNegaImg, arcImg, clashEggImg, function(){
   eggcanvas.width = w;
   eggcanvas.height = h;
 
-  // TODO set target. hit and change
   drawMe.geza1();
-  // drawMe.geza2();
-  // drawMe.geza3();
-  // drawMe.geza4();
-  // drawMe.geza5();
-  // drawMe.geza6();
 
   var images;
 
@@ -141,20 +135,27 @@ loadImages(eggPosiImg, eggNegaImg, arcImg, clashEggImg, function(){
   function changeMe(){
     c.clearRect(0, 0, w, h);
     me++;
-    drawMe['geza'+me]();
+    if (me < 7) {
+      drawMe['geza'+me]();
+    } else {
+      drawYoroshiku();
+    }
+  }
+
+  function drawYoroshiku(){
+    c.font = "20px sans-serif";
+    c.fillText('＼よろしくお願い致します！／', w/2, h/2 - 200);
   }
 
   function throwEgg(egg){
     // loadImages(eggPosiImg, eggNegaImg, arcImg, clashEggImg, function(){
       images = [eggPosiImg, arcImg, eggNegaImg, arcImg];
-      // var posiSize = eggPosiImg.width();
       var i = 0;
       var tick = 0;
       var hitWidth;
       var hitHeight;
       var hitTick;
       eggc.globalAlpha = 1.0;
-      if (egg.x < 0) { egg.x * -1; }
       var rad;
       var once = true;
 
@@ -167,13 +168,14 @@ console.log(egg);
         var time2;
         eggc.clearRect(0, 0, w, h);
 
-        rad = tick * (Math.PI / 180) * 10 + egg.x;
+        rad = tick * (Math.PI / 180) * 15 + egg.x;
+        var gravity = (150 * Math.sin(rad));
         if (0.9 - (tick * 0.05) > 0) {
 
           if (images[i] === arcImg) {
-            eggc.drawImage(images[i], w/2.5 + (6 * tick), h/2.5 * 1.1 - (200 * Math.sin(rad)), images[i].width * (0.9 - (tick * 0.05)), images[i].height * (0.9 - (tick * 0.05)));
+            eggc.drawImage(images[i], w/2.5 + (6 * tick), h/2.5 * 1.1 - gravity , images[i].width * (0.9 - (tick * 0.05)), images[i].height * (0.9 - (tick * 0.05)));
           } else {
-            eggc.drawImage(images[i], w/2.5 + (6 * tick), h/2.5 - (200 * Math.sin(rad)), images[i].width * (0.9 - (tick * 0.05)), images[i].height * (0.9 - (tick * 0.05)));
+            eggc.drawImage(images[i], w/2.5 + (6 * tick), h/2.5 - gravity, images[i].width * (0.9 - (tick * 0.05)), images[i].height * (0.9 - (tick * 0.05)));
 
           }
         } else if (0.9 - (tick * 0.05) === 0){
@@ -181,7 +183,7 @@ console.log(egg);
           hitWidth = w/2.5 + (6 * tick);
           hitTick = tick;
           eggc.globalAlpha = (hitTick / (tick - hitTick) * 1.2) - 0.5;
-          eggc.drawImage(clashEggImg, hitWidth - clashEggImg.width* 0.2 / 2, h/2.5 - (200 * Math.sin(rad)), clashEggImg.width * 0.2, clashEggImg.height * 0.2);
+          eggc.drawImage(clashEggImg, hitWidth - clashEggImg.width* 0.2 / 2, h/2.5 - gravity, clashEggImg.width * 0.2, clashEggImg.height * 0.2);
         } else {
           eggc.globalAlpha = (hitTick / (tick - hitTick) * 1.2) - 0.5;
           eggc.drawImage(clashEggImg, hitWidth - clashEggImg.width* 0.2 / 2, hitHeight + (tick - hitTick), clashEggImg.width * 0.2, clashEggImg.height * 0.2);
