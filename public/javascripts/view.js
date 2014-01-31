02c7c6767013f53caa1a0803af52e94f2a67e388
Yuuki Shichiku <yuuki.79@gmail.com>
2014-02-01 00:59:51 +0900

    Add egg demow

--- public/javascripts/view.js
+++ public/javascripts/view.js
@@ -1,9 +1,9 @@
 var socket = io.connect('http://bapa-egg.herokuapp.com');
 //var socket = io.connect('http://localhost:3000/');
 var s = socket.of('/socket');
-s.on('connect', function(msg) {
-  console.log('connected');
-});
+// s.on('connect', function(msg) {
+//   console.log('connected');
+// });
 
 var canvas = document.getElementById("main");
 var c = canvas.getContext("2d");
