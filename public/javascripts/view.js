97cd19d45e2bed4719128ede4288e1396a20b5a7
Yuki Shichiku <yuuki.79@gmail.com>
2014-02-02 15:07:55 +0900

    Fixed load images

--- public/javascripts/view.js
+++ public/javascripts/view.js
@@ -1,5 +1,5 @@
-//var socket = io.connect('http://bapa-egg.herokuapp.com');
-var socket = io.connect('http://localhost:3000/');
+var socket = io.connect('http://bapa-egg.herokuapp.com');
+//var socket = io.connect('http://localhost:3000/');
 var s = socket.of('/socket');
 // s.on('connect', function(msg) {
 //   console.log('connected');
